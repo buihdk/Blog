3464bc7a7136926a85369c56e25b2d660e5c51d1
Khoa Bui <buihdk@gmail.com>
2016-10-06 05:49:52 +0700

    third commit with comment feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,19 +2,17 @@ class CommentsController < ApplicationController
   before_action :find_article
 
 	def create
-    @comment = @article.comments.create(params[:comment].permit(:content))
-    #@comment.user_id = current_user.id
-    @comment.save
-
-    if @comment.save
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
 	end
 
-  private
-  def find_article
-    @article = Article.find(params[:article_id])
+  def comment_params
+    params.require(:comment).permit(:content)
   end
+
+  private
+    def find_article
+      @article = Article.find(params[:article_id])
+    end
+
 end
--- db/migrate/20161004123329_create_comments.rb
+++ db/migrate/20161004123329_create_comments.rb
@@ -3,7 +3,6 @@ class CreateComments < ActiveRecord::Migration[5.0]
     create_table :comments do |t|
       t.text :content
       t.references :article, foreign_key: true
-      t.references :user, foreign_key: true
 
       t.timestamps
     end
